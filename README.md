4c3cd518f5b3dc90181062d152154d24fcddce7f
Josh Vickery <jcv8000@gmail.com>
2022-11-22 15:21:11 -0600

    show how to edit preload restarting

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# electron-vite-ts ⚡
+# electron-vite-ts
 
 Vite + Electron + TypeScript + ESLint Template (React by default) with Hot Reloading for main, preload, and renderer
+
+## Preload hot reloading
+
+By default I have the `watch.mjs` script set up so the entire app process gets killed and restarted whenever the `preload` is modified.
+
+If you want to change this, edit `scripts/watch.mjs` in the `watchPreload` function to say:
+
+```ts
+plugins: [{
+    name: "electron-preload-watcher",
+    closeBundle() {
+        server.ws.send({ type: "full-reload" }); // instead of restart();
+    }
+}],
+```
--- scripts/watch.mjs
+++ scripts/watch.mjs
@@ -2,13 +2,13 @@ import { spawn } from "child_process";
 import { createServer, build } from "vite";
 import electron from "electron";
 
-/** @type {import('node:child_process').ChildProcess} */
+/** @type {import("node:child_process").ChildProcess} */
 let electronApp = null;
 let killable = false;
 
 function watchMain() {
 
-    /** @type {import('net').AddressInfo} */
+    /** @type {import("net").AddressInfo} */
     const address = server.httpServer.address();
     Object.assign(process.env, {
         VITE_DEV_SERVER_HOSTNAME: address.address,
@@ -33,13 +33,19 @@ function watchMain() {
     });
 }
 
-function watchPreload() {
+/**
+ * @param {import("vite").ViteDevServer} server 
+ */
+function watchPreload(server) {
     return build({
         configFile: "packages/preload/vite.config.ts",
         mode: "development",
         plugins: [{
             name: "electron-preload-watcher",
             closeBundle() {
+                // If you don't want the entire app to shut down and restart
+                // when the preload is modified, replace the line below with
+                // server.ws.send({ type: "full-reload" });
                 restart();
             }
         }],
@@ -68,7 +74,7 @@ function restart() {
 const server = await createServer({ configFile: "packages/renderer/vite.config.ts" });
 
 await server.listen();
-await watchPreload();
+await watchPreload(server);
 await watchMain();
 
 start();
