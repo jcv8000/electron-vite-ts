21e2a5a37dd342c48cd0c0d15367bae2843dbf6a
Josh Vickery <jcv8000@gmail.com>
2022-11-25 17:40:00 -0600

    fix electron starting early in watch script

--- scripts/watch.mjs
+++ scripts/watch.mjs
@@ -2,58 +2,9 @@ import { spawn } from "child_process";
 import { createServer, build } from "vite";
 import electron from "electron";
 
-/** @type {import("node:child_process").ChildProcess} */
+/** @type {import('node:child_process').ChildProcess} */
 let electronApp = null;
 let killable = false;
-
-function watchMain() {
-
-    /** @type {import("net").AddressInfo} */
-    const address = server.httpServer.address();
-    Object.assign(process.env, {
-        VITE_DEV_SERVER_HOSTNAME: address.address,
-        VITE_DEV_SERVER_PORT: address.port,
-        VITE_DEV_SERVER_URL: `http://${address.address}:${address.port}`
-    });
-
-    return build({
-        configFile: "packages/main/vite.config.ts",
-        mode: "development",
-        plugins: [
-            {
-                name: "electron-main-watcher",
-                closeBundle() {
-                    restart();
-                }
-            }
-        ],
-        build: {
-            watch: {}
-        }
-    });
-}
-
-/**
- * @param {import("vite").ViteDevServer} server 
- */
-function watchPreload(server) {
-    return build({
-        configFile: "packages/preload/vite.config.ts",
-        mode: "development",
-        plugins: [{
-            name: "electron-preload-watcher",
-            closeBundle() {
-                // If you don't want the entire app to shut down and restart
-                // when the preload is modified, replace the line below with
-                // server.ws.send({ type: "full-reload" });
-                restart();
-            }
-        }],
-        build: {
-            watch: {}
-        }
-    });
-}
 
 function start() {
     electronApp = spawn(electron, ["."], { stdio: "inherit" });
@@ -71,14 +22,53 @@ function restart() {
     }
 }
 
-const server = await createServer({ configFile: "packages/renderer/vite.config.ts" });
+// Preload
+await build({
+    configFile: "packages/preload/vite.config.ts",
+    mode: "development",
+    clearScreen: false,
+    plugins: [{
+        name: "electron-preload-watcher",
+        closeBundle() {
+            // If you don't want the entire app to shut down and restart
+            // when the preload is modified, replace the line below with
+            // server.ws.send({ type: "full-reload" });
+            restart();
+        }
+    }],
+    build: {
+        watch: {}
+    }
+});
 
+// Main
+await build({
+    configFile: "packages/main/vite.config.ts",
+    mode: "development",
+    clearScreen: false,
+    plugins: [
+        {
+            name: "electron-main-watcher",
+            closeBundle() {
+                restart();
+            }
+        }
+    ],
+    build: {
+        watch: {}
+    }
+});
+
+const server = await createServer({ configFile: "packages/renderer/vite.config.ts" });
 await server.listen();
-await watchPreload(server);
-await watchMain();
+
+/** @type {import('net').AddressInfo} */
+const address = server.httpServer.address();
+Object.assign(process.env, {
+    VITE_DEV_SERVER_HOSTNAME: address.address,
+    VITE_DEV_SERVER_PORT: address.port,
+    VITE_DEV_SERVER_URL: `http://${address.address}:${address.port}`
+});
 
 start();
-
-setTimeout(() => {
-    killable = true;
-}, 700);
+killable = true;
